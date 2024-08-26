e7e0f9dae1c84346b2a9326c802db76b6929cc56
HARISH USIRIKAYALA <106104183+Harish-chintu7777777@users.noreply.github.com>
2024-08-26 17:53:14 +0530

    Delete Construct Binary Tree from Preorder and Postorder Traversal.py

--- Construct Binary Tree from Preorder and Postorder Traversal.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def constructFromPrePost(self, preorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
-        root = TreeNode(postorder.pop())
-        if root.val != preorder[-1]:
-            root.right = self.constructFromPrePost(preorder, postorder)
-        if root.val != preorder[-1]:
-            root.left = self.constructFromPrePost(preorder, postorder)
-        preorder.pop()
-        return root
